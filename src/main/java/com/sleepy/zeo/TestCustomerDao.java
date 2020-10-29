3236f3ff1bcc8b8012016aed84f057aaac40348f
Vaadhoo <steven199409@outlook.com>
2020-10-30 02:42:46 +0800

    maaaaac

--- src/main/java/com/sleepy/zeo/TestCustomerDao.java
+++ src/main/java/com/sleepy/zeo/TestCustomerDao.java
@@ -13,26 +13,26 @@ import java.util.List;
 public class TestCustomerDao {
 
     public static void main(String[] args) throws Exception {
-        //1.加载SqlMapConfig.xml
+        // 加载mybatis_config.xml
         InputStream in = Resources.getResourceAsStream("mybatis_config.xml");
 
-        //2.创建SqlSessionFactory工厂
+        // 创建SqlSessionFactory工厂
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(in);
 
-        //3.创建SqlSession
+        // 创建SqlSession
         SqlSession sqlSession = factory.openSession();
 
-        //4.生成Dao接口代理对象
+        // 生成Dao接口代理对象
         CustomerDao userDao = sqlSession.getMapper(CustomerDao.class);
 
-        //5.执行Dao接口方法
+        // 执行Dao接口方法
         List<CustomerBean> list = userDao.fetchAllCustomers();
         for (CustomerBean c : list) {
             System.out.println(c);
         }
 
-        //6.释放资源
+        // 释放资源
         sqlSession.close();
         in.close();
     }
